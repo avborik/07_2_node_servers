8407d96f000be9489b889c84a5d1e0a15225a6c6
AvBorikDeveloper <avboris28@gmail.com>
2019-05-19 15:44:37 +1200

    Basic server setup

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
 const http = require('http');
+const fs = require('fs');
+let HTML = fs.readFileSync('./index.html');
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-type':'application/json'});
+        const json = JSON.stringify({ 
+            name:'Boris',
+            cars:['Ford','Nissan']
+        })
+    res.end(json.toString())
+})
+server.listen(8181,'127.0.0.1');
+console.log('server is running on port 8181')
 
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-type':'text/plain'});
-    res.end('Hello Bear, i am response')
-}).listen(8181,'127.0.0.1');
+
+// res.write(`
+//     <html>
+//         <body>
+//             <h1 style="background:red">Hello there</h1>
+//         </body>
+//     </html>
+//     `)
+
+// const server = http.createServer((req,res)=>{
+//     res.writeHead(200,{'Content-type':'text/html'});
+//     res.end(HTML)
+// })
+
+// res.end(HTML)
