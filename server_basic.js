d268be3d30e19ee2f5b2a7a12103cf3ca18ed641
AvBorikDeveloper <avboris28@gmail.com>
2019-05-19 22:16:28 +1200

    routing

--- server_basic.js
+++ server_basic.js
@@ -1,13 +1,23 @@
 const http = require('http');
 const fs = require('fs');
-let HTML = fs.readFileSync('./index.html');
+
 const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-type':'application/json'});
+
+    if(req.url === "/"){
+        res.writeHead(200,{'Content-type':'text/html'});
+        let HTML = fs.readFileSync('./index.html');
+        res.end(HTML)
+    }else if (req.url === "/api/user"){
+        res.writeHead(200,{'Content-type':'application/json'});
         const json = JSON.stringify({ 
             name:'Boris',
             cars:['Ford','Nissan']
         })
-    res.end(json.toString())
+        res.end(json)
+    } else {
+        res.writeHead(404)
+        res.end()
+    }
 })
 server.listen(8181,'127.0.0.1');
 console.log('server is running on port 8181')
